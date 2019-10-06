0514de28f5c139bfb4c0a738e02a8fc048126804
Fabrício Barbosa Viegas <49844765+fabriciobarbosaviegas@users.noreply.github.com>
2019-10-06 11:36:33 -0300

    Update main.py

--- snake py/main.py
+++ snake py/main.py
@@ -16,7 +16,7 @@ LEFT = 3
 
 pygame.init()
 screen = pygame.display.set_mode((400,400))
-pygame.display.set_caption('Snake py')
+pygame.display.set_caption('Snake')
 
 snake = [(200,200), (210,200), (220,200)]
 snake_skin = pygame.Surface((10,10))
@@ -33,8 +33,38 @@ clock = pygame.time.Clock()
 font = pygame.font.Font('freesansbold.ttf', 18)
 score = 0
 game_over = False
+
 while not game_over:
-    clock.tick(10)
+
+    if score > 20 and score < 40:
+        snake_skin.fill((0,250,100))
+        clock.tick(12)
+        print('Facíl 2')
+
+    elif score > 40 and score < 60:
+        snake_skin.fill((0,250,200))
+        clock.tick(14)
+        print('Médio 1')
+
+    elif score > 60 and score < 80:
+        snake_skin.fill((0,250,250))
+        clock.tick(16)
+        print('Médio 2')
+
+    elif score > 80 and score < 100:
+        snake_skin.fill((100,250,250))
+        clock.tick(18)
+        print('Dificíl 1')
+
+    elif score > 100:
+        snake_skin.fill((200,250,250))
+        clock.tick(20)
+        print('Dificíl 2')
+
+    else:
+        clock.tick(10)    
+        print('Facíl 1')
+
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
@@ -42,12 +72,15 @@ while not game_over:
         if event.type == KEYDOWN:
             if event.key == K_UP:
                 my_direction = UP
+
         if event.type == KEYDOWN:
             if event.key == K_DOWN:
                 my_direction = DOWN
+
         if event.type == KEYDOWN:
             if event.key == K_LEFT:
                 my_direction = LEFT
+
         if event.type == KEYDOWN:
             if event.key == K_RIGHT:
                 my_direction = RIGTH
@@ -56,19 +89,27 @@ while not game_over:
         apple_pos = on_grid_random()
         snake.append((0,0))
         score += 1
+
     if snake[0][0] == 400 or snake[0][1] == 400 or snake[0][0] < 0 or snake[0][1] < 0:
+        print('Colisão Borda')
         game_over = True
         print('Game Over')
         print(f'total de pontos: {score}')
+
         break
+
     for i in range(len(snake) -1, 0, -1):
         snake[i] = (snake[i-1][0], snake[i-1][1])
+
     if my_direction == UP:
         snake[0] = (snake[0][0], snake[0][1] -10)
+
     if my_direction == DOWN:
         snake[0] = (snake[0][0], snake[0][1] +10)
+
     if my_direction == RIGTH:
         snake[0] = (snake[0][0] +10, snake[0][1])
+
     if my_direction == LEFT:
         snake[0] = (snake[0][0] -10, snake[0][1])
 
